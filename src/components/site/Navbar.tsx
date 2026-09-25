50060d051a2534175f4df37d12aa18f02f110d1c
Ilesanmi Emmanuel Ayodeji <fixbaa.tech@gmail.com>
2026-09-25 13:56:24 +0100

    Update Navbar.tsx

--- src/components/site/Navbar.tsx
+++ src/components/site/Navbar.tsx
@@ -59,7 +59,7 @@ export function Navbar() {
               to={link.to}
               activeOptions={{ exact: link.to === "/" }}
               className="rounded-full px-4 py-2.5 text-sm font-medium text-white/80 transition-colors hover:bg-white/10 hover:text-white"
-              activeProps={{ className: "text-[#022866] bg-white" }}
+              activeProps={{ className: "!text-[#022866] bg-white" }}
             >
               {link.label}
             </Link>
