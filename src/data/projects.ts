80f4b9022ed7b6eaea096e44555ff3561a6750e3
fixbaatech <fixbaa.tech@gmail.com>
2026-09-26 23:35:58 +0100

    Update projects.ts

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -14,7 +14,7 @@ import kotopoImage from "@/assets/achievers-borehole-kotopo.png";
 import obadaImage from "@/assets/achievers-borehole-obada.png";
 import obantokoImage from "@/assets/achievers-borehole-obantoko.png";
 import schoolBoreholeDrilling from "@/assets/school-borehole.png";
-import waterGushing from "@/assets/schoolBoreholeDrilling ";
+import waterGushing from "@/assets/schoolBoreholeDrilling";
 
 export type Project = {
   slug: string;
